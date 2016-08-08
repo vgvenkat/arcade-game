eb7b6de99aeac193fe5f11e4ec2658018d9d2211
Venkataraman Valady Ganesan <vgkrish619@gmail.com>
2016-08-07 22:39:33 -0700

    Adding more functionality to the game, bugs and player

--- README.md
+++ README.md
@@ -4,6 +4,18 @@ This is a simple frogger game with a human and bugs, intention is to reach the w
 
 Built with html5 canvas and OO javascript.
 
+#### How to Run this game
+
+#### How to play this game
+
+
 #### License
 
-The codebase is open, have fun.
+Same license as the [base project](https://github.com/udacity/frontend-nanodegree-arcade-game)
+
+frontend-nanodegree-arcade-game
+===============================
+
+Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission. Make sure the functions you write are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the **readme.md** file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
+
+For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
--- js/app.js
+++ js/app.js
@@ -1,11 +1,15 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = x;
+    this.y = y;
+    //this.location = ;
+    this.speed = speed;
 };
 
 // Update the enemy's position, required method for game
@@ -14,21 +18,75 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    // console.log(dt);
+     this.x = this.x +this.speed* dt ;
+     //this.y = this.y * dt;
+
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
+
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+Enemy.prototype.collision = function() {
+
+}
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function(x, y) {
+  this.sprite = 'images/char-boy.png';
+  this.x = x;
+  this.y = y;
+}
+Player.prototype.update = function(dt) {
+  // console.log(x,y);
+  // this.x += x;
+  // this.y += y;
+  //if y == -50 then reset
+  //  this.x = this.x +this.x* dt ;
+  //  this.y = this.y +this.y* dt ;
+}
+
+Player.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+Player.prototype.handleInput = function(direction) {
+  //setup row and column sizes
+  //6 rows
+  //5 columns
+  //all centered
+  // x 1 - 0, 100, 200, 300, 400 change by 100
+  //y - -50, 40, 130, 220, 310, 400 change by 90
 
 
+  switch(direction) {
+    case 'up' : if (this.y < 0) break;
+                else this.y -= 90; break;
+    case 'down': if (this.y == 400) break;
+                else this.y += 90; break;
+    case 'right' : if (this.x == 400) break;
+                  else this.x += 100; break;
+    case 'left' : if(this.x == 0) break;
+                  else this.x -=100; break;
+  }
+
+
+}
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var bug1 = new Enemy(30, 130, 100);
+var bug2 = new Enemy(50, 210, 300);
+var bug3 = new Enemy(30, 60, 50);
+var bug4 = new Enemy(20, 130, 500);
+
+var player = new Player(200, 400);
+
+var allEnemies = [bug1, bug2, bug3, bug4];
 
 
 
